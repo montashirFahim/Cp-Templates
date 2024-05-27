aadbcb13da2ac5d3347a7f94da2f60c319adffd6
montashirFahim <89032441+montashirFahim@users.noreply.github.com>
2024-05-28 05:31:38 +0600

    Update SegTree.cpp

--- Data Structure/SegTree.cpp
+++ Data Structure/SegTree.cpp
@@ -1,34 +1,102 @@
-ll a[N + 5];
-struct segmenttree {
-	ll seg[4 * N + 5];
-	void build(int node, int l, int r) {
-		if (r == l) {
-			seg[node] = 1;
-			return;
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+template <class T> class SumSegmentTree {
+private:
+	const T DEFAULT = 0;
+	vector<T> segtree;
+	int len;
+
+public:
+	SumSegmentTree(int len) : len(len), segtree(len * 2, DEFAULT) {}
+	void set(int ind, T val) {
+		ind += len;
+		segtree[ind] = val;
+		for (; ind > 1; ind /= 2) {
+			segtree[ind / 2] = segtree[ind] + segtree[ind ^ 1];
 		}
-		int mid = (r + l) / 2;
-		build(2 * node, l, mid);
-		build(2 * node + 1, mid + 1, r);
-		seg[node] = seg[2 * node] + seg[2 * node + 1];
 	}
-
-	void update_point(int node, int st, int en, int pos) {
-		if (st == en) {
-			seg[node] = 0;
-			return;
+	T range_sum(int start, int end) {
+		T sum = DEFAULT;
+		for (start += len, end += len; start < end; start /= 2, end /= 2) {
+			if (start % 2 == 1) { sum += segtree[start++]; }
+			if (end % 2 == 1) { sum += segtree[--end]; }
 		}
-		int mid = (st + en) / 2;
-		if (pos <= mid)update_point(2 * node, st, mid, pos);
-		else update_point(2 * node + 1, mid + 1, en, pos);
-		seg[node] = seg[2 * node] + seg[2 * node + 1];
-	}
-
-
-	ll query(int node, int st, int en, int l, int r) {
-		if (en < l || r < st)return 0;
-		if (l <= st && en <= r)return seg[node];
-		int mid = (st + en) / 2;
-		return query(2 * node, st, mid, l, r) + query(2 * node + 1, mid + 1, en, l, r);
+		return sum;
 	}
 };
 
+template <class T> class MinSegmentTree {
+private:
+	const T DEFAULT = INT64_MAX;
+	vector<T> segtree;
+	int len;
+
+public:
+	MinSegmentTree(int len) : len(len), segtree(len * 2, DEFAULT) {}
+	/** Sets the value at ind to val. */
+	void set(int ind, T val) {
+		ind += len;
+		segtree[ind] = val;
+		for (; ind > 1; ind /= 2) {
+			segtree[ind / 2] = std::min(segtree[ind], segtree[ind ^ 1]);
+		}
+	}
+	/** @return the minimum element in the range [start, end) */
+	T range_min(int start, int end) {
+		T min = DEFAULT;
+		for (start += len, end += len; start < end; start /= 2, end /= 2) {
+			if (start % 2 == 1) { min = std::min(min, segtree[start++]); }
+			if (end % 2 == 1) { min = std::min(min, segtree[--end]); }
+		}
+		return min;
+	}
+};
+
+template <class T> class MaxSegmentTree {
+private:
+	const T DEFAULT = 0;
+	vector<T> segtree;
+	int len;
+
+public:
+	MaxSegmentTree(int len) : len(len), segtree(len * 2, DEFAULT) {}
+	/** Sets the value at ind to val. */
+	void set(int ind, T val) {
+		ind += len;
+		segtree[ind] = val;
+		for (; ind > 1; ind /= 2) {
+			segtree[ind / 2] = std::max(segtree[ind], segtree[ind ^ 1]);
+		}
+	}
+	/** @return the maximum element in the range [start, end) */
+	T range_max(int start, int end) {
+		T max = DEFAULT;
+		for (start += len, end += len; start < end; start /= 2, end /= 2) {
+			if (start % 2 == 1) {max = std::max(max, segtree[start++]); }
+			if (end % 2 == 1) { max = std::max(max, segtree[--end]); }
+		}
+		return max;
+	}
+};
+
+
+int main() {
+	ll n, q;
+	cin >> n >> q;
+	SumSegmentTree<ll>s1(n);
+	MaxSegmentTree<ll>s2(n);
+	MinSegmentTree<ll>s3(n);
+	for (int i = 0; i < n; i++) {
+		ll x;
+		cin >> x;
+		s1.set(i, x);
+		s2.set(i, x);
+		s3.set(i, x);
+	}
+	while (q--) {
+
+	}
+	return 0;
+}
