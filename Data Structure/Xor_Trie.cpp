5f4d7b557a9aa3e202a7cc8c02d63681b7501feb
montashirFahim <89032441+montashirFahim@users.noreply.github.com>
2024-10-18 01:10:42 +0600

    Update Xor_Trie.cpp

--- Data Structure/Xor_Trie.cpp
+++ Data Structure/Xor_Trie.cpp
@@ -1,105 +1,121 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
+const ll mod = 1e9 + 7;
+const int MX = 31 * 500005;
 
-class Trie {
-	//N is way big then don't use struct and trie and cnt declare as global array
-	//Don't use as Class
-	int num, B = 30;
-	vector<vector<int> >trie;
-	vector<int>cnt;
-public:
-	Trie() {}
-	void init() {
-		num = 0;
-		cnt.clear(), trie.clear();
-		trie.push_back(vector<int>(2)), cnt.push_back(0);
-		trie.back()[0] = trie.back()[1] = -1;
-	}
-	void insert(int val) {
-		int node = 0;
+int num, B = 30;
+int cnt[MX], trie[MX][2];
+
+void init() {
+	num = 0;
+	memset(trie, -1, sizeof(trie));
+}
+void insert(int val) {
+	int node = 0;
+	cnt[node]++;
+	for (int i = B; i >= 0; i--) {
+		int k = val >> i & 1;
+		if (trie[node][k] == -1) {
+			trie[node][k] = ++num;
+		}
+		node = trie[node][k];
 		cnt[node]++;
-		for (int i = B; i >= 0; i--) {
-			int k = val >> i & 1;
-			if (trie[node][k] == -1) {
-				trie.push_back(vector<int>(2)), cnt.push_back(0);
-				trie.back()[0] = trie.back()[1] = -1;
-				trie[node][k] = ++num;
-			}
+	}
+}
+void del(int val) {
+	int node = 0;
+	cnt[node]--;
+	for (int i = B; i >= 0; i--) {
+		int k = val >> i & 1;
+		if (trie[node][k] == -1) {
+			break;
+		}
+		int prev = node;
+		node = trie[node][k];
+		cnt[node]--;
+		if (cnt[node] == 0) {
+			trie[prev][k] = -1;
+		}
+	}
+}
+//number of subarray such that xor(a[i])<k
+ll query(int val, int k) {
+	ll ans = 0;
+	int node = 0;
+	for (int i = B; i >= 0; i--) {
+		if (node == -1)break;
+		int b1 = val >> i & 1, b2 = k >> i & 1;
+		if (b2 == 1) {
+			if (trie[node][b1] != -1)ans += (ll)cnt[trie[node][b1]];
+			node = trie[node][!b1];
+		} else {
+			node = trie[node][b1];
+		}
+	}
+	return ans;
+}
+int get_max(int val) {
+	int node = 0, ans = 0;
+	for (int i = B; i >= 0; i--) {
+		int k = val >> i & 1;
+		if (trie[node][!k] != -1) {
+			ans <<= 1; ans++;
+			node = trie[node][!k];
+		} else {
+			ans <<= 1;
 			node = trie[node][k];
-			cnt[node]++;
 		}
 	}
-	//number of subarray such that xor(a[i])<k
-	ll query(int val, int k) {
-		ll ans = 0;
-		int node = 0;
-		for (int i = B; i >= 0; i--) {
-			if (node == -1)break;
-			int b1 = val >> i & 1, b2 = k >> i & 1;
-			if (b2 == 1) {
-				if (trie[node][b1] != -1)ans += (ll)cnt[trie[node][b1]];
-				node = trie[node][!b1];
-			} else {
-				node = trie[node][b1];
-			}
+	return ans;
+}
+int get_min(int val) {
+	int node = 0, ans = 0;
+	for (int i = B; i >= 0; i--) {
+		int k = val >> i & 1;
+		if (trie[node][k] != -1) {
+			ans <<= 1;
+			node = trie[node][k];
+		} else {
+			ans <<= 1; ans++;
+			node = trie[node][!k];
 		}
-		return ans;
 	}
-	int get_max(int val) {
-		int node = 0, ans = 0;
-		for (int i = B; i >= 0; i--) {
-			int k = val >> i & 1;
-			if (trie[node][!k] != -1) {
-				ans <<= 1; ans++;
-				node = trie[node][!k];
-			} else {
-				ans <<= 1;
-				node = trie[node][k];
-			}
-		}
-		return ans;
-	}
-	int get_min(int val) {
-		int node = 0, ans = 0;
-		for (int i = B; i >= 0; i--) {
-			int k = val >> i & 1;
-			if (trie[node][k] != -1) {
-				ans <<= 1;
-				node = trie[node][k];
-			} else {
-				ans <<= 1; ans++;
-				node = trie[node][!k];
-			}
-		}
-		return ans;
-	}
-};
+	return ans;
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	int t;
-	cin >> t;
-	for (int tc = 1; tc <= t; tc++) {
-		int n, k;
-		cin >> n >> k;
-		int cur = 0;
-		Trie tr;
-		tr.init();
-		tr.insert(0);
-		int mx = 0, mn = 1e9 + 2;
-		ll ans = (n * (n + 1)) / 2;
-		for (int i = 0; i < n; i++) {
-			int x;
-			cin >> x;
-			cur ^= x;
-			mx = max(mx, tr.get_max(cur));
-			mn = min(mn, tr.get_min(cur));
-			ans -= tr.query(cur, k);
-			tr.insert(cur);
+	int q;
+	cin >> q;
+	init();
+	//becareful about input format if operation 3 will not be done when set is empty then no need to add 0
+	insert(0);
+	map<int, int>mp;
+	for (int i = 0; i < q; i++) {
+		assert(num <= MX);
+		char x;
+		cin >> x;
+		if (x == '+') {
+			int val;
+			cin >> val;
+			if (mp.find(val) == mp.end()) {
+				insert(val);
+			}
+			mp[val]++;
+		} else if (x == '-') {
+			int val;
+			cin >> val;
+			mp[val]--;
+			if (mp[val] == 0) {
+				mp.erase(val);
+				del(val);
+			}
+		} else if (x == '?') {
+			int val;
+			cin >> val;
+			cout << get_max(val) << "\n";
 		}
-		cout << "Case " << tc << ": ";
-		cout << ans << " " << mx << " " << mn << "\n";
 	}
 }
